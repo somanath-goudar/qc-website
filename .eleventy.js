78b29702066f363d95ba3d7bca596a99f90bfafd
Somanath <somugowda8050@gmail.com>
2022-08-17 15:13:25 +0530

    fixed cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/styles.css')
-  eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addPassthroughCopy("./src/styles.css")
+  eleventyConfig.addPassthroughCopy("./src/assets")
+  eleventyConfig.addPassthroughCopy("./src/admin")
 
   return {
     dir: {
-      input: 'src',
-      output: 'public',
+      input: "src",
+      output: "public",
     },
   }
 }
